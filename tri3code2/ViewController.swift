325afa3116dd1d08a0008865c0b3dd26de606528
aryan114 <amberkararyan@gmail.com>
2020-03-11 09:09:23 -0700

    Initial Commit

---
+++ tri3code2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tri3code2
+//
+//  Created by Amberkar, Aryan on 3/11/20.
+//  Copyright © 2020 Amberkar, Aryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
